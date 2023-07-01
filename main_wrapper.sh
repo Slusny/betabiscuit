d107fba71938f89daf56a0446a6b142e636b9960
Lennart Slusny <lennart@slusny.de>
2023-07-01 03:07:12 +0200

    slurm

---
+++ main_wrapper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pip -V
+pip3 -V
+python -V
+python3 -V
+
+# pip3 install -r requirements_pip.txt 
+# python3 -m pip install git+https://github.com/martius-lab/laser-hockey-env.git
+# python3 main.py --algo ddpg --env pendulum --wandb
