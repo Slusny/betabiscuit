bdd9057ef81ffba6f92a16479492a16ab4600938
lennytubby <34861751+lennytubby@users.noreply.github.com>
2023-07-01 14:06:19 +0200

    Update main_wrapper.sh

--- main_wrapper.sh
+++ main_wrapper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-get install swig
 pip install -r requirements_pip.txt 
 python -m pip install git+https://github.com/martius-lab/laser-hockey-env.git
 python main.py --algo ddpg --env pendulum --wandb
