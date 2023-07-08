c60dc0d5545af31065cab383cd8ea9ef3708fa56
lennytubby <lennart@slusny.de>
2023-07-08 23:04:33 +0200

    test

--- test/test_virtual_display.py
+++ test/test_virtual_display.py
@@ -5,8 +5,13 @@ from importlib import reload
 import time
 import matplotlib.pyplot as plt
 # %matplotlib inline
-from IPython import display
+# from IPython import display
+import pyvirtualdisplay
 
+
+_display = pyvirtualdisplay.Display(visible=False,  # use False with Xvfb
+                                    size=(1400, 900))
+_ = _display.start()
 
 env = h_env.HockeyEnv()
 player1 = h_env.BasicOpponent()
@@ -15,13 +20,14 @@ obs_buffer = []
 reward_buffer=[]
 obs, info = env.reset()
 obs_agent2 = env.obs_agent_two()
-plt.figure()
-img = plt.imshow(env.render(mode='rgb_array'))
-plt.show()
+# plt.figure()
+# img = plt.imshow(env.render(mode='rgb_array'))
+env.render()
 for k in range(100):
-    img.set_data(env.render(mode='rgb_array'))
-    display.display(plt.gcf())
-    display.clear_output(wait=True)
+    env.render()
+    # img.set_data(env.render(mode='rgb_array'))
+    # display.display(plt.gcf())
+    # display.clear_output(wait=True)
     a1 = player1.act(obs)
     a2 = player2.act(obs_agent2)
     obs, r, d, _, info = env.step(np.hstack([a1,a2]))    
