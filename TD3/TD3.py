7e1b93a8afef993dc624862326234eae34ee5461
lennytubby <lennart@slusny.de>
2023-08-04 01:48:01 +0200

    fix

--- TD3/TD3.py
+++ TD3/TD3.py
@@ -341,7 +341,7 @@ class TD3Agent(object):
                 if self._config["bc"]:
                     alpha = self._config["bc_lambda"]/q.abs().mean().detach()
                     a_teacher = to_torch(np.array([self.teacher.act(s_elem.cpu().numpy()) for s_elem in s ])) # expensive copy back and forth
-                    actor_loss += - alpha * torch.mean(q) + nn.functional.mse_loss(a_policy,a_teacher)
+                    actor_loss = - alpha * torch.mean(q) + nn.functional.mse_loss(a_policy,a_teacher)
                 else:
                     actor_loss = -torch.mean(q)
                 actor_loss.backward()
