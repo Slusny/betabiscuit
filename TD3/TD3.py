880030cc01b37cce16cf40ad3608d8484d943378
lennytubby <lennart@slusny.de>
2023-08-07 13:16:56 +0200

    f

--- TD3/TD3.py
+++ TD3/TD3.py
@@ -466,7 +466,7 @@ class TD3Agent(object):
         return losses
     
     def save_agent_wandb(self, i_episode, rewards, lengths, losses,notes=""):
-        save_checkpoint(self.state(),self.savepath,"TD3",self.env_name, i_episode, self.wandb_run, self._eps, None, self.seed,rewards,lengths, losses,notes)
+        save_checkpoint(self.state(),self.savepath,"TD3",self.env_name, i_episode, self.wandb_run, self._eps, None, self.seed,rewards,lengths, losses,notes=notes)
 
     def opponent_action(self):
         if (self.env_name == "hockey"):
