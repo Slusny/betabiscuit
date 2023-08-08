cb5bdcdf3007d784b35be13a82147da2e3d29750
lennytubby <lennart@slusny.de>
2023-08-08 21:05:37 +0200

    f

--- selfplay_eval.py
+++ selfplay_eval.py
@@ -203,6 +203,7 @@ def validate(agents,names, idx1, idx2,val_episodes,max_timesteps,visualize,sleep
         total_reward=0
         agent1_touch_puck = []
         agent2_touch_puck = []
+        touches = []
         for t in range(max_timesteps):
 
             if visualize: env.render()
@@ -230,11 +231,14 @@ def validate(agents,names, idx1, idx2,val_episodes,max_timesteps,visualize,sleep
         # print("agent 1 touches: ",sum(agent2_touch_puck))
         rewards.append(total_reward)
         length.append(t)
+        touches.append((sum(agent1_touch_puck) + sum(agent2_touch_puck)) > 0.0)
     win_rate = np.array(rewards)
     draw_rate = np.sum(win_rate == 0) / win_rate.size
+    draw_rate_with_touch = ((win_rate[touches] + 1) / 2).mean().round(2)
+    touch_rate = round(sum(touches) / len(touches),2)
     win_rate = (win_rate + 1 ) /2
     win_rate = win_rate.mean().round(3)
-    print("\t win rate ",names[idx1], " vs ",names[idx2], ": ",np.round(win_rate,2), " - draws: ",draw_rate, " max length: ",np.array(length).max(), " avg length: ",np.array(length).mean())
+    print("\t win rate ",names[idx1], " vs ",names[idx2], ": ",np.round(win_rate,2), " - draws: ",draw_rate, ", touch_rate: ",touch_rate, ", draw_rate_with_touch: ",draw_rate_with_touch, " max length: ",np.array(length).max(), " avg length: ",np.array(length).mean())
     return win_rate, draw_rate
 
 def validation(args_main,agents, config_agents, names, env,val_episodes,visualize,sleep,maxtimesteps):
