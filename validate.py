10b7a1fcec68aff959ef1f99604706e7bcc13969
lennytubby <lennart@slusny.de>
2023-08-11 18:45:25 +0200

    f

--- validate.py
+++ validate.py
@@ -3,12 +3,24 @@ from run_model import opponent_action
 from run_model import add_derivative
 from run_model import parse_arguments_and_get_agent
 from run_model import run
+from argparse import Namespace
 
-run_args = {
+run_args = Namespace(**{
     "validate":True,
-    "local_config":"./wandb/latest-run/config.yaml",
+    "local_config":"validation/agents/TD3.json",
     "boostrap_local":True,
     "bootstrap":"validation/agents/TD3.pth"
-}
+    "vir":False,
+    "max_episodes": 100
+    "max_timesteps": 250,
+    "sleep": 0.,
+    "legacy":False,
+    "action_n":4,
+    "validate":True
+    "weak_opponent":False,
+    "project":False,
+    "run_name":"latest",
+    "run_id":"latest"
+})
 agent,env,args,string = parse_arguments_and_get_agent(run_args)
 run(run_args,[agent],env,[args],[string])
