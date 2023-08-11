b30df7c8fb5566fb45f456a79ea8966b24cda011
lennytubby <lennart@slusny.de>
2023-08-11 18:49:50 +0200

    f

--- validate.py
+++ validate.py
@@ -5,7 +5,8 @@ from run_model import parse_arguments_and_get_agent
 from run_model import run
 from argparse import Namespace
 
-run_args = {
+run_args_list =
+Namespace(**{
     "validate":True,
     "local_config":"validation/agents/TD3.json",
     "bootstrap_local":True,
@@ -21,7 +22,32 @@ run_args = {
     "project":False,
     "run_name":"latest",
     "run_id":"latest"
-}
-run_args = Namespace(**run_args)
-agent,env,args,string = parse_arguments_and_get_agent(run_args)
-run(run_args,[agent],env,[args],[string])
+}),
+Namespace(**{
+    "validate":True,
+    "local_config":"validation/agents/TD3.json",
+    "bootstrap_local":True,
+    "bootstrap":"validation/agents/TD3.pth",
+    "vir":False,
+    "max_episodes": 100,
+    "max_timesteps": 250,
+    "sleep": 0.,
+    "legacy":False,
+    "action_n":4,
+    "validate":True,
+    "weak_opponent":False,
+    "project":False,
+    "run_name":"latest",
+    "run_id":"latest"
+})]
+
+agents = []
+args_list = []
+strings = []
+for run_args in run_args_list:
+    agent,env,args,string = parse_arguments_and_get_agent(run_args)
+    agents.append(agent)
+    args_list.append(args)
+    strings.append(string)
+
+run(run_args,agents,env,args_list,strings)
