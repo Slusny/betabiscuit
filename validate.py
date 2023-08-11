def552f05a92ee8d949142b21bc042693d3e94e2
lennytubby <lennart@slusny.de>
2023-08-11 19:20:03 +0200

    f

--- validate.py
+++ validate.py
@@ -5,7 +5,7 @@ from run_model import parse_arguments_and_get_agent
 from run_model import run
 from argparse import Namespace
 
-MAX_EPISODES = 100
+MAX_EPISODES = 200
 
 run_args_list_strong = [
 Namespace(**{
@@ -123,6 +123,128 @@ Namespace(**{
     "action_n":8,
     "validate":True,
     "weak_opponent":False,
+    "project":False,
+    "run_name":"latest",
+    "run_id":"latest"
+})
+]
+
+run_args_list_weak = [
+Namespace(**{
+    "validate":True,
+    "local_config":"validation/agents/TD3.json",
+    "bootstrap_local":True,
+    "bootstrap":"validation/agents/TD3.pth",
+    "vir":False,
+    "max_episodes": MAX_EPISODES,
+    "max_timesteps": 250,
+    "sleep": 0.,
+    "legacy":False,
+    "action_n":4,
+    "validate":True,
+    "weak_opponent":True,
+    "project":False,
+    "run_name":"latest",
+    "run_id":"latest"
+}),
+Namespace(**{
+    "validate":True,
+    "local_config":"validation/agents/TD3_tournament.json",
+    "bootstrap_local":True,
+    "bootstrap":"validation/agents/TD3_tournament.pth",
+    "vir":False,
+    "max_episodes": MAX_EPISODES,
+    "max_timesteps": 250,
+    "sleep": 0.,
+    "legacy":False,
+    "action_n":4,
+    "validate":True,
+    "weak_opponent":True,
+    "project":False,
+    "run_name":"latest",
+    "run_id":"latest"
+}),
+Namespace(**{
+    "validate":True,
+    "local_config":"validation/agents/DDQN_acceleration.json",
+    "bootstrap_local":True,
+    "bootstrap":"validation/agents/DDQN_acceleration.pth",
+    "vir":False,
+    "max_episodes": MAX_EPISODES,
+    "max_timesteps": 250,
+    "sleep": 0.,
+    "legacy":False,
+    "action_n":4,
+    "validate":True,
+    "weak_opponent":True,
+    "project":False,
+    "run_name":"latest",
+    "run_id":"latest"
+}),
+Namespace(**{
+    "validate":True,
+    "local_config":"validation/agents/DDQN_tournament.json",
+    "bootstrap_local":True,
+    "bootstrap":"validation/agents/DDQN_tournament.pth",
+    "vir":False,
+    "max_episodes": MAX_EPISODES,
+    "max_timesteps": 250,
+    "sleep": 0.,
+    "legacy":False,
+    "action_n":4,
+    "validate":True,
+    "weak_opponent":True,
+    "project":False,
+    "run_name":"latest",
+    "run_id":"latest"
+}),
+Namespace(**{
+    "validate":True,
+    "local_config":"validation/agents/DDPG.json",
+    "bootstrap_local":True,
+    "bootstrap":"validation/agents/DDPG.pth",
+    "vir":False,
+    "max_episodes": MAX_EPISODES,
+    "max_timesteps": 250,
+    "sleep": 0.,
+    "legacy":True,
+    "action_n":8,
+    "validate":True,
+    "weak_opponent":True,
+    "project":False,
+    "run_name":"latest",
+    "run_id":"latest"
+}),
+Namespace(**{
+    "validate":True,
+    "local_config":"validation/agents/DDPG_BC.json",
+    "bootstrap_local":True,
+    "bootstrap":"validation/agents/DDPG_BC.pth",
+    "vir":False,
+    "max_episodes": MAX_EPISODES,
+    "max_timesteps": 250,
+    "sleep": 0.,
+    "legacy":True,
+    "action_n":8,
+    "validate":True,
+    "weak_opponent":True,
+    "project":False,
+    "run_name":"latest",
+    "run_id":"latest"
+}),
+Namespace(**{
+    "validate":True,
+    "local_config":"validation/agents/DDPG_dense.json",
+    "bootstrap_local":True,
+    "bootstrap":"validation/agents/DDPG_dense.pth",
+    "vir":False,
+    "max_episodes": MAX_EPISODES,
+    "max_timesteps": 250,
+    "sleep": 0.,
+    "legacy":True,
+    "action_n":8,
+    "validate":True,
+    "weak_opponent":True,
     "project":False,
     "run_name":"latest",
     "run_id":"latest"
