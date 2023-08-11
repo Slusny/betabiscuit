d30118cd24fb599bed2e6f4ad9c8a652e697d335
lennytubby <lennart@slusny.de>
2023-08-11 19:01:04 +0200

    f

--- DDPG/DDPG.py
+++ DDPG/DDPG.py
@@ -1,4 +1,5 @@
 import torch
+import collections
 import sys
 import torch.nn as nn
 from torch.distributions import MultivariateNormal
@@ -126,6 +127,7 @@ class DDPGAgent(object):
             "bc_lambda":2.0,
             "cpu": False,
             "replay_ratio": 0,
+            "bootstrap_local":False,
 
         }
         self._config.update(userconfig)
@@ -178,13 +180,20 @@ class DDPGAgent(object):
         # To resume training from a saved model.
         # Models get saved in weights-and-biases and loaded from there.
         if(self._config["bootstrap"] is not None):
-            api = wandb.Api()
-            art = api.artifact(self._config["bootstrap"], type='model')
-            if self._config["cpu"]:
-                state = torch.load(art.file(),map_location='cpu')
+            if self._config["bootstrap_local"]:
+                state = torch.load(str(self._config["bootstrap"]))
+                if isinstance(state, collections.OrderedDict):
+                    self.restore_state(state)
+                else:
+                    self.Q = state
             else:
-                state = torch.load(art.file())
-            self.restore_state(state)
+                api = wandb.Api()
+                art = api.artifact(self._config["bootstrap"], type='model')
+                if self._config["cpu"]:
+                    state = torch.load(art.file(),map_location='cpu')
+                else:
+                    state = torch.load(art.file())
+                self.restore_state(state)
 
         if self._config["bc"]:
             self.teacher = h_env.BasicOpponent(weak=False)
